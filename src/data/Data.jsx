600d29aa17e61bc44ee41bafa6b4996417b964f3
nhuthq <nhuthq@zigexn.vn>
2025-03-07 12:50:36 +0700

    add assets images

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -11,4 +11,53 @@ const Pricing = [
   { id: 4, amount: "$25000" },
 ];
 
+const CarCategories = [
+  {
+    id: 1,
+    name: "Micro",
+    icon: "/src/assets/images/micro.png",
+  },
+  {
+    id: 2,
+    name: "Sedan",
+    icon: "/src/assets/images/sedan.png",
+  },
+  {
+    id: 3,
+    name: "SUV",
+    icon: "/src/assets/images/suv.png",
+  },
+  {
+    id: 4,
+    name: "Hatchback",
+    icon: "/src/assets/images/hatchback.png",
+  },
+  {
+    id: 5,
+    name: "Pickup",
+    icon: "/src/assets/images/pickup.png",
+  },
+  {
+    id: 6,
+    name: "Adventure",
+    icon: "/src/assets/images/adventure.png",
+  },
+  {
+    id: 7,
+    name: "Convertible",
+    icon: "/src/assets/images/convertible.png",
+  },
+
+  {
+    id: 8,
+    name: "Super",
+    icon: "/src/assets/images/super.png",
+  },
+  {
+    id: 9,
+    name: "Luxury",
+    icon: "/src/assets/images/luxury.png",
+  },
+];
+
 export { CarModel, Pricing };
