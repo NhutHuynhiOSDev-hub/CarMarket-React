86a8661316ad7725198680519d9230a6a71d073d
nhuthq <nhuthq@zigexn.vn>
2025-03-08 16:52:39 +0700

    finish

--- src/lib/header/Header.jsx
+++ src/lib/header/Header.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function Header() {
   const { isSigned } = useUser();
   return (
-    <div className="flex justify-between items-center p-5 shadow-sm">
+    <div className="flex justify-between items-center py-5 px-10 shadow-sm">
       <img src="vite.svg" className="logo" alt="logo" />
       <ul className="hidden md:flex gap-16">
         <li className="font-medium hover:scale-110 hover:text-gray-400 transition-all cursor-pointer hover:text-primary">
