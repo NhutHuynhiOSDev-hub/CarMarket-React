deb2872655b7303bf6fe6c2e7a02bf0365b8c685
nhuthq <nhuthq@zigexn.vn>
2025-03-16 14:03:18 +0700

    finish

--- src/data/FakerData.jsx
+++ src/data/FakerData.jsx
@@ -10,6 +10,11 @@ function CreateDummyList() {
       "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/bmw-330e-review-2024-01.jpg?itok=gna2hmk3",
     miles: 1000,
     gearType: "Automatic",
+    createBy: "nhuthq@zigexn.vn",
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    justArrived: faker.datatype.boolean(),
+    inStock: faker.datatype.boolean(),
     price: faker.finance.amount(100000, 500000, 0),
   };
 }
